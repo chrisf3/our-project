681d572d63f00c77f20b0119f38c5c4e8d9237c4
chrisf3 <c.formula1@gmail.com>
2018-12-11 11:33:27 +0100

    here we go again

--- oop_first_classes_example.py
+++ oop_first_classes_example.py
@@ -1,5 +1,11 @@
-class cat():
-  def__init__(self, name, owner, color):
-    self.name=name
-    self.owner=owner
-    self.color=color
+#oop first classes example
+class cat:
+  def __init__(self, name, owner, color):
+    self.name = name
+    self.owner = owner
+    self.color = color
+cat_1=cat('Clocharde','Hannah','multicolored')
+cat_2=cat('Leia','Paula','orange')
+
+
+print(cat_1.color)
